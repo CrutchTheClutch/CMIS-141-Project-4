8913a6509a3f14c6cefece8999d857aa889e6e66
CrutchtheClutch <halogamefreak81@gmail.com>
2017-07-27 21:59:51 -0700

    Reformatted Code
    Added comments, removed unnecessary mehtods and redundant variables

--- Project 4/titanic/TestTitanic.java
+++ Project 4/titanic/TestTitanic.java
@@ -1,97 +1,80 @@
 package titanic;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
-/*
-* File: Titanic.java
-* Author: William Crutchfield
-* Date: March 3, 2016
-* Purpose: This program will test the Titanic class
-*/
-
+/**
+ * File: Titanic.java
+ * Author: William Crutchfield
+ * Date: March 3, 2016
+ * Description: Opens the "Titanic.txt" file and reads all of the data into the program.
+ * Then, allows the user to get various statics about the Titanic based on this data.
+ */
 public class TestTitanic {
     public static void main(String[] args) throws IOException {
         
-        //Variables
+        // Variables
         String input;
         int i = 0;
         Scanner scannerIn = new Scanner(System.in);
         
-        //Commandline Argument Check
+        // Commandline Argument Check
         if (args.length == 1) {
             Titanic titanic = new Titanic(args[0]);
             System.out.print(titanic.getHeader());
         
-            //Main Loop
+            // Main Loop
             while (i <= 0) {
                 System.out.print(titanic.getMenu());
                 input = scannerIn.next();
                 input = input.toUpperCase();
                 System.out.println("");
-                
-        
-                if (input.equals("1")) {
-                    System.out.print("There were " + titanic.getTotalPassengers() + " Passengers on the Titanic.");
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("2")) {
-                    System.out.print(titanic.getPerishedPassengers() + "% of the Passengers on the Titanic Perished.");
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("3")) {
-                    System.out.print(titanic.getSurvivedPassengers() + "% of the Passengers on the Titanic Survived.");
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("4")) {
-                    System.out.print(titanic.getSurvivedClass1() + "% of the Passengers in 1st class Survived.");
-                    System.out.print("\n" + titanic.getSurvivedClass2() + "% of the Passengers in 2nd class Survived.");
-                    System.out.print("\n" + titanic.getSurvivedClass3() + "% of the Passengers in 3rd class Survived.");
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("5")) {
-                    System.out.println("A list of passengers under the age of 10 that Survived the sinking of the Titanic.");
-                    System.out.print(titanic.get10yrSurvived());
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("6")) {
-                    System.out.print("Enter a letter: ");
-                    input = scannerIn.next();
-                    char ch = input.charAt(0);
-                    System.out.println("Count: " + titanic.getFirstLetterLastName(ch));
-                    System.out.println("");
-                }
-                else if (input.equals("7")) {
-                    System.out.print(titanic.getMenSurvived() + "% of the Men on the Titanic Survived.");
-                    System.out.println("");
-                    System.out.println("");                    
-                }
-                else if (input.equals("8")) {
-                    System.out.print(titanic.getWomenSurvived() + "% of the Women on the Titanic Survived.");
-                    System.out.println("");
-                    System.out.println("");
-                }
-                else if (input.equals("Q")) {
-                    i++;
+
+                // Prints response based on input
+                switch (input) {
+                    case "1":
+                        System.out.println("There were " + titanic.getTotalPassengers() + " Passengers on the Titanic.");
+                        break;
+                    case "2":
+                        System.out.println(titanic.getPerishedPassengers() + "% of the Passengers on the Titanic Perished.");
+                        break;
+                    case "3":
+                        System.out.println(titanic.getSurvivedPassengers() + "% of the Passengers on the Titanic Survived.");
+                        break;
+                    case "4":
+                        System.out.println(titanic.getSurvivedClass1() + "% of the Passengers in 1st class Survived.");
+                        System.out.println(titanic.getSurvivedClass2() + "% of the Passengers in 2nd class Survived.");
+                        System.out.println(titanic.getSurvivedClass3() + "% of the Passengers in 3rd class Survived.");
+                        break;
+                    case "5":
+                        System.out.println("A list of passengers under the age of 10 that Survived the sinking of the Titanic.");
+                        System.out.println(titanic.get10yrSurvived());
+                        break;
+                    case "6":
+                        System.out.print("Enter a letter: ");
+                        input = scannerIn.next();
+                        char ch = input.charAt(0);
+                        System.out.println("Count: " + titanic.getFirstLetterLastName(ch));
+                        break;
+                    case "7":
+                        System.out.println(titanic.getMenSurvived() + "% of the Men on the Titanic Survived.");
+                        break;
+                    case "8":
+                        System.out.println(titanic.getWomenSurvived() + "% of the Women on the Titanic Survived.");
+                        break;
+                    case "Q":
+                        i++;
+                        break;
                 }
             }
             System.out.print(titanic.getExit());
-            System.out.print(titanic.getTimer());
-            System.out.println(titanic.getElapsedTime());
             System.exit(0);
         }
-        else {
-            Titanic titanic = new Titanic();
-            System.out.print(titanic.getHeader());
-            System.out.print(titanic.getNoArgs());
-            System.exit(0);
-        }
+
+        // Prints this if no commandline arguments
+        Titanic titanic = new Titanic();
+        System.out.print(titanic.getHeader());
+        System.out.print(titanic.getNoArgs());
+        System.exit(0);
     }
 }
-//System.out.print(Arrays.deepToString(titanic.getArray()));
--- Project 4/titanic/Titanic.java
+++ Project 4/titanic/Titanic.java
@@ -1,91 +1,61 @@
 package titanic;
 
-/*
-* File: Titanic.java
-* Author: William Crutchfield
-* Date: March 3, 2016
-* Purpose: This program will make the Titanic class and also 
-* provide information about the passengers on the Titanic
-*/
-
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.text.DecimalFormat;
 
-public class Titanic {
-    
-//// Variables ////
-    
-    //Arg Variables
-    private String fileName = "";
-    
-    //Timer Variables
-    private final long tStart;
-    private long tEnd;
-    private long tDelta;
-    private long elapsedSeconds;
-    
-    //Array Variables
-    private BufferedReader br;
-    private String[][] array;
-    private int row;
-    private int col;
-    private String thisLine;
-    private String[] temp;
-    //private String arrayPrint;
+/**
+ * File: Titanic.java
+ * Author: William Crutchfield
+ * Date: March 3, 2016
+ * Description: Reads in data from a text file with information about the Titanic. Then, processes that data
+ * to gather various statistics about the Titanic.
+ */
+class Titanic {
 
-    
-    
-//// End of Variables ////
+    // Variables
+    private final long tStart;  // timer start variable, used to calculate elapsed time
+    private String[][] array;   // used to store data from txt file
+    private int row;            // counter variable for the amount of rows in the txt file
 
-
-//// Constructors ////
-    
-    public Titanic() {
+    /**
+     * Default Constructor for Titanic
+     */
+    Titanic() {
         this.tStart = System.currentTimeMillis();
-        this.fileName = "";
     }
-    public Titanic(String fileName) throws IOException {
+
+    /**
+     * Constructor that takes a fileName as an argument. Designed to be used with the included Titanic.txt file.
+     * @param fileName name of the text file to be read in (Titanic.txt)
+     * @throws IOException is thrown if file is not found
+     */
+    Titanic(String fileName) throws IOException {
         this.tStart = System.currentTimeMillis();
-        this.fileName = fileName;
-        br = new BufferedReader(new FileReader(fileName));
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
         array = new String[1309][6];
         
         for (row = 0; row < array.length; row++) {
-            thisLine = br.readLine();
-            temp = thisLine.split("\t", -1);
-            for (col = 0; col < array[row].length; col++) {
-                array[row][col] = temp[col];
-            }
+            String thisLine = br.readLine();
+            String[] temp = thisLine.split("\t", -1);
+            System.arraycopy(temp, 0, array[row], 0, array[row].length);
         }
     }
 
-//// End of Constructors ////
-
-
-//// Array Methods ////
-    
-    //getArray Method
-    public String[][] getArray() throws IOException {
-        for (row = 0; row < array.length; row++){
-            for (col = 0; col < array[row].length; col++){
-                System.out.print(array[row][col] + "  ");
-            }
-        System.out.println();
-        }
-        br.close();
-        String[][] noReturn = {{"End of Titanic Array"}};
-        return noReturn;
+    /**
+     * Calculates the total number of passengers on the Titanic
+     * @return number of passengers
+     */
+    int getTotalPassengers() {
+        return array.length;
     }
-    
-    //getTotalPassengers Method
-    public int getTotalPassengers() {
-        int getTotalPassengers = array.length;
-        return getTotalPassengers;
-    }
-    //getPerishedPassengers Method
-    public double getPerishedPassengers() {
+
+    /**
+     * Calculates the total number of passengers that perished on the Titanic
+     * @return number of perished passengers
+     */
+    double getPerishedPassengers() {
         int alive = 0;
         for (row = 0; row < array.length; row++){
             if(array[row][1].equals("1")){
@@ -95,13 +65,16 @@ public class Titanic {
         int total = array.length;
         int decrease = total - alive;
         double percent1 = ((double)decrease) / total;
-        double percent2  = percent1 * 100;
-        double getPerishedPassengers = percent2;
+        double getPerishedPassengers = percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getPerishedPassengers));
     }
-    //getSurvivedPassengers Method
-    public double getSurvivedPassengers() {
+
+    /**
+     * Calculates the total number of passengers that survived on the Titanic
+     * @return number of survived passengers
+     */
+    double getSurvivedPassengers() {
         int dead = 0;
         for (row = 0; row < array.length; row++){
             if(array[row][1].equals("0")){
@@ -111,15 +84,16 @@ public class Titanic {
         int total = array.length;
         int decrease = total - dead;
         double percent1 = ((double)decrease) / total;
-        double percent2  = percent1 * 100;
-        double getSurvivedPassengers = percent2;
+        double getSurvivedPassengers = percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getSurvivedPassengers));
     }
-    
-    //// getSurvivedClasses Methods ////
-    //getSurvivedClass1 Method
-    public double getSurvivedClass1() {
+
+    /**
+     * Calculates the total number of passengers that survived on the Titanic that were in Class 1
+     * @return number of class 1 survived passengers
+     */
+    double getSurvivedClass1() {
         int class1Dead = 0;
         int class1Total = 0;        
         for (row = 0; row < array.length; row++){
@@ -134,13 +108,16 @@ public class Titanic {
         }    
         int class1Decrease = class1Total - class1Dead;
         double class1Percent1 = ((double)class1Decrease) / class1Total;
-        double class1Percent2  = class1Percent1 * 100;
-        double getSurvivedClass1 = class1Percent2;
+        double getSurvivedClass1 = class1Percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getSurvivedClass1));
     }
-    //getSurvivedClass2 Method
-    public double getSurvivedClass2() {
+
+    /**
+     * Calculates the total number of passengers that survived on the Titanic that were in Class 2
+     * @return number of class 2 survived passengers
+     */
+    double getSurvivedClass2() {
         int class2Dead = 0;
         int class2Total = 0;        
         for (row = 0; row < array.length; row++){
@@ -155,13 +132,16 @@ public class Titanic {
         }    
         int class2Decrease = class2Total - class2Dead;
         double class2Percent1 = ((double)class2Decrease) / class2Total;
-        double class2Percent2  = class2Percent1 * 100;
-        double getSurvivedClass2 = class2Percent2;
+        double getSurvivedClass2 = class2Percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getSurvivedClass2));
     }
-    //getSurvivedClass1 Method
-    public double getSurvivedClass3() {
+
+    /**
+     * Calculates the total number of passengers that survived on the Titanic that were in Class 3
+     * @return number of class 3 survived passengers
+     */
+    double getSurvivedClass3() {
         int class3Dead = 0;
         int class3Total = 0;        
         for (row = 0; row < array.length; row++){
@@ -176,14 +156,16 @@ public class Titanic {
         }    
         int class3Decrease = class3Total - class3Dead;
         double class3Percent1 = ((double)class3Decrease) / class3Total;
-        double class3Percent2  = class3Percent1 * 100;
-        double getSurvivedClass3 = class3Percent2;
+        double getSurvivedClass3 = class3Percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getSurvivedClass3));
     }
 
-    //getMenSurvived Method
-    public double getMenSurvived() {
+    /**
+     * Calculates the total number of men that survived on the Titanic
+     * @return number of men survived
+     */
+    double getMenSurvived() {
         int dead = 0;
         int total = 0;        
         for (row = 0; row < array.length; row++){
@@ -198,13 +180,16 @@ public class Titanic {
         }    
         int decrease = total - dead;
         double percent1 = ((double)decrease) / total;
-        double percent2  = percent1 * 100;
-        double getMenSurvived = percent2;
+        double getMenSurvived = percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getMenSurvived));
     }
-    //getWomenSurvived Method
-    public double getWomenSurvived() {
+
+    /**
+     * Calculates the total number of women that survived on the Titanic
+     * @return number of women survived
+     */
+    double getWomenSurvived() {
         int dead = 0;
         int total = 0;        
         for (row = 0; row < array.length; row++){
@@ -219,30 +204,37 @@ public class Titanic {
         }    
         int decrease = total - dead;
         double percent1 = ((double)decrease) / total;
-        double percent2  = percent1 * 100;
-        double getWomenSurvived = percent2;
+        double getWomenSurvived = percent1 * 100;
         DecimalFormat twoDForm = new DecimalFormat("#.##");        
         return Double.valueOf(twoDForm.format(getWomenSurvived));
     }
-    
-    //get10yrSurvived Method
-    public String get10yrSurvived() {
-        String name = "";
+
+    /**
+     * Calculates the total number of children under the age of 10 that survived on the Titanic
+     * @return number of children under the age of 10 survived
+     */
+    String get10yrSurvived() {
+        StringBuilder name = new StringBuilder();
         for (row = 0; row < array.length; row++) {
             if (array[row].length >= 6 && array[row][4].length() > 0) {
                 double pAge = Double.parseDouble(array[row][4]);
                 if (array[row][1].equals("1")){
                     if (pAge < 10) {
-                    name += "\n" + array[row][2];
+                    name.append("\n").append(array[row][2]);
                     }
                 }
             }
         }
-        return name;
+        return name.toString();
     }
-    
-    //getFirstLetterLastName Method
-    public int getFirstLetterLastName(char ch) {
+
+    /**
+     * Calculates the total number of passengers on the Titanic that has
+     * the char entered as the first letter in their last name
+     * @param ch first letter in last name
+     * @return number of passengers with the char as the first letter in last name
+     */
+    int getFirstLetterLastName(char ch) {
         int count = 0;
         for (row = 0; row < array.length; row++) {
             char first = array[row][2].charAt(0);
@@ -252,20 +244,21 @@ public class Titanic {
         }
         return count;
     }
-    
-//// End of Array Methods ////
-    
-    
-//// Graphics User Interface Methods ////
-    
-    //getHeader Method
-    public String getHeader() {
-        String getHeader = ("********** Welcome to the Titanic Statistical Application **************************");
-        return getHeader;
+
+    /**
+     * Header String for program, used for formatting purposes
+     * @return header string
+     */
+    String getHeader() {
+        return ("********** Welcome to the Titanic Statistical Application **************************");
     }
-    //getMenu Method
-    public String getMenu() {
-        String getMenu = ("\n" +
+
+    /**
+     * Menu toString method, is called so the user knows which input will answer which question
+     * @return menu string
+     */
+    String getMenu() {
+        return ("\n" +
                 "\nEnter the number of the question you want answered. Enter 'Q' to quit the program :" +
                 "\n" +
                 "\n1.\tHow many passengers were on the Titanic?" +
@@ -279,44 +272,35 @@ public class Titanic {
                 "\nQ.\tQuit the program" +
                 "\n" +
                 "\nEnter your selection: ");
-        return getMenu;
     }
-    //getExit Method
-    public String getExit() {
-        String getExit = ("Thank you for trying the Titanic Program!");
-        return getExit;
-    }
-    //getTimer Method
-    public String getTimer() {
-        String getTimer = ("\n" +
-                "\nElapsed time in seconds was: ");
-        return getTimer;
-    }
-    //getNoArgs Method
-    public String getNoArgs() {
-        String getNoArgs = ("\n" + 
-                "\n There was an Error!" + 
+
+    /**
+     * Is called if the program is run without a commandline argument
+     * @return String alerting the user to use a commandline argument
+     */
+    String getNoArgs() {
+        return ("\n" +
+                "\n There was an Error!" +
                 "\n" +
                 "\n You must have use a commandline argument when launching this program.");
-        return getNoArgs;
     }
-    
-//// End of Graphics User Interface Methods ////
 
-
-//// Timer Methods ////
-
-    //getElapsedTime Method
-    public long getElapsedTime() {
-        tEnd = System.currentTimeMillis();
-        tDelta = tEnd - tStart;
-        elapsedSeconds = tDelta / 1000;
-        long getElapsedTime = elapsedSeconds;
-        return getElapsedTime;
+    /**
+     * Displays the exit message along with the elapsed time
+     * @return exit message
+     */
+    String getExit() {
+        return ("Thank you for trying the Titanic Program!" +
+                "\nElapsed time in seconds was: " + getElapsedTime());
     }
-    
-//// End of Timer Methods ////
 
-
-  
+    /**
+     * Tracks the amount of time, in seconds the program is running
+     * @return elapsed time in seconds
+     */
+    private long getElapsedTime() {
+        long tEnd = System.currentTimeMillis();
+        long tDelta = tEnd - tStart;
+        return tDelta / 1000;
+    }
 }
